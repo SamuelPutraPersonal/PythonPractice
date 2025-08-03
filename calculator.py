39358eb91a82407b0edf94b3f060018ff733f5b7
Samuel Putra <samueldianpermanaputra@gmail.com>
2025-08-03 23:20:04 +0200

    Just try to change it

--- calculator.py
+++ calculator.py
@@ -1,27 +1,31 @@
-# calculator 
+# calculator
 def add(num1, num2):
-    " this function to add two numbers and return the result"
+    "this function to add two numbers and return the result"
     result = num1 + num2
     return result
 
-def subtract(num1,num2):
-    " this function to subsctract two numbers and return the result"
+
+def subtract(num1, num2):
+    "this function to subsctract two numbers and return the result"
     result = num1 - num2
     return result
 
+
 def multiply(num1, num2):
-    " this function to multiply two numbers and return the result"
+    "this function to multiply two numbers and return the result"
     result = num1 * num2
     return result
 
+
 def divide(num1, num2):
-    " this function to divide two numbers and return the result"
-    if num2 ==0:
-       print("Error: cannot divide by zero")
-       return None # Indicate an invalid result
+    "this function to divide two numbers and return the result"
+    if num2 == 0:
+        print("Error: cannot divide by zero")
+        return None  # Indicate an invalid result
     else:
-        result = num1 /num2
+        result = num1 / num2
         return result
+
 
 print("Welcome to the calculator program")
 num1 = int(input("Enter number one = "))
@@ -29,15 +33,14 @@ num2 = int(input("Enter number two = "))
 print("1. Add\n2. Subtract\n3. Multiply\n4. Divide")
 
 
-
-# give the user choices to be chosen 
+# give the user choices to be chosen
 choice = int(input("Enter your choice: "))
 if choice == 1:
     print(add(num1, num2))
 elif choice == 2:
     print(substract(num1, num2))
 elif choice == 3:
-    print(multiply(num1,num2))
+    print(multiply(num1, num2))
 elif choice == 4:
     print(divide(num1, num2))
 else:
